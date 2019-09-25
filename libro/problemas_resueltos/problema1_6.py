bfbf78e311215df50277222c7897298172e8e04b
astrxtmtz21 <jairt359@gmail.com>
2019-09-25 00:16:10 -0600

    Problemas resueltos del 1.1 al 1.8

---
+++ libro/problemas_resueltos/problema1_6.py
@@ -0,0 +1,3 @@
+DIAS = int(input("Numero de dias:"))
+SEG = DIAS * 24 * 60 * 60
+print(f"En { DIAS } hay { SEG } segundos")
---
+++ libro/problemas_resueltos/problema1_8.py
@@ -0,0 +1,6 @@
+X1 = float(input("Coordenada del punto P1 en X:"))
+Y1 = float(input("Coordenada del punto P1 en Y:"))
+X2 = float(input("Coordenada del punto P2 en X:"))
+Y2 = float(input("Coordenada del punto P2 en Y:"))
+DIS = ((X1 - X2) ** 2 + (Y1 - Y2) ** 2) ** 0.5
+print(f"La distancia resultante entre los dos puntos es { DIS } dadas dichas coordenadas")
