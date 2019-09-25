0af7b26e31c7ccd717f494e0f3dfaf305c464586
astrxtmtz21 <jairt359@gmail.com>
2019-09-25 00:09:15 -0600

    Ejercicios resueltos del 1.1 al 1.8 del libro

---
+++ libro/problema1_4.py
@@ -0,0 +1,3 @@
+GAL = float(input("Numero de galones utilizados:"))
+GAL = GAL * 3.785 * 8.20
+print(f"El cliente debe pagar { GAL } ya que cada galon tiene 3.785 litros y el precio del litro es $8.20")
